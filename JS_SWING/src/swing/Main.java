f9ce97c7aac2f1add254bb126f7c0dd6c3e4afa3
강재석 <kjsbest007@naver.com>
2023-03-06 11:23:10 +0900

    initial commit

---
+++ JS_SWING/src/swing/Main.java
@@ -0,0 +1,11 @@
+package swing;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyFrame myframe = new MyFrame("자바 응용");
+		myframe.setTitle("나의 프레임");
+	}
+
+}
